b5e3a642474565d85aff108b51d0476f389d1d64
David Kim <davidskim02@gmail.com>
2026-06-04 19:25:04 -0400

    basic procedural map generation

--- src/GameScene.cs
+++ src/GameScene.cs
@@ -5,9 +5,9 @@ using System.Diagnostics;
 public partial class GameScene : Node
 {
 	private MapController mapController;
-	private readonly int width = 16;
-	private readonly int height = 8;
-	private readonly float hexSize = 40f;
+	private readonly int width = 42;
+	private readonly int height = 24;
+	private readonly float hexSize = 20f;
 	
 	public override void _Ready() {
 		HexGrid hexGrid = new HexGrid(width, height);
